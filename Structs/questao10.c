907a1e74a78779187b6a34ca87b650b6e030e904
Duda Rodrigues <147636488+Duda-Rodrigues-Contato@users.noreply.github.com>
2025-09-18 20:10:08 -0300

    Testando questao 10 (nao terminei!!)

---
+++ Structs/questao10.c
@@ -0,0 +1,117 @@
+/*
+    QUESTÃO 10:
+
+    Crie uma struct chamada ContaBancaria, contendo:
+        • Nome do titular (string de até 50 caracteres)
+        • Número da conta (inteiro)
+        • Saldo (float)
+    Crie funções para:
+        • Depositar um valor na conta
+        • Sacar um valor da conta (apenas se houver saldo suficiente)
+        • Exibir o saldo
+    No main(), crie uma conta bancária e permita que o usuário realize operações.
+
+*/
+
+#include <stdio.h>
+
+
+typedef struct ContaBancaria {
+
+    char nomeTitular[50];
+    int numConta;
+    float saldo;
+
+} ContaBancaria;
+
+
+void depositar(float saldo) {
+
+    float valorDepoito;
+
+    printf("Digite o valor a ser depositado: \n");
+    scanf("%f", &valorDepoito);
+
+    saldo += valorDepoito;
+
+    printf("Deposito realizado com sucesso! \n");
+
+    return saldo;
+
+}
+
+
+void sacarValor(float saldo) {
+
+    float valorSacar;
+    float testeSaldo;
+
+    printf("Digite o valor de saque: \n");
+    scanf("%f", &valorSacar);
+
+    testeSaldo = saldo - valorSacar;
+
+    if (testeSaldo > 0) {
+        saldo = testeSaldo;
+        printf("Saldo realizado com sucesso!\n");
+    } else {
+        printf("Saldo insuficiente!\n");
+    }
+
+    return saldo;
+
+}
+
+
+void exibirSaldo(float saldo) {
+
+    printf("Seu saldo atual e de: R$ %.2f", saldo);
+
+}
+
+
+int main() {
+
+    ContaBancaria conta;
+
+    int escolhaUsuario;
+    conta.saldo = 0;
+    char escolhaContinuar = 'S';
+
+    printf("Digite o nome do titular da conta: ");
+    scanf(" %[^\n]", conta.nomeTitular);
+
+    printf("Digite o numero da conta: ");
+    scanf("%d", &conta.numConta);
+
+    printf("Digite o saldo inicial: ");
+    scanf("%f", &conta.saldo);
+
+    printf("Conta criada com sucesso! \n");
+
+    while (escolhaContinuar == 's' || escolhaContinuar == 'S') {
+
+        printf("Escolha a operacao que deseja realizar: \n");
+        printf("1 - Depositar\n");
+        printf("2 - Sacar\n");
+        printf("3 - Exibir Saldo\n");
+        scanf("%d", &escolhaUsuario);
+
+        if (escolhaUsuario == 1) {
+            depositar(conta.saldo);
+        } else if (escolhaUsuario == 2) {
+            sacarValor(conta.saldo);
+        } else if (escolhaUsuario == 3) {
+            exibirSaldo(conta.saldo);
+        } else {
+            printf("Escolha um valor valido!\n");
+        }
+
+        printf("Você deseja realizar outra operacao? (s/n)\n");
+        scanf(" %c", &escolhaContinuar);
+
+    }
+
+    return 0;
+
+}

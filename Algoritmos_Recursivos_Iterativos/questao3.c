b06452df2c3c69053eac0025ea685d58c3e4554b
Duda Rodrigues <147636488+Duda-Rodrigues-Contato@users.noreply.github.com>
2025-09-23 13:09:44 -0300

    Estruturando a questão 3.

---
+++ Algoritmos_Recursivos_Iterativos/questao3.c
@@ -0,0 +1,19 @@
+/*
+    QUESTÃO 3:
+
+    Implemente uma função recursiva que recebe um número inteiro n e retorna a
+    soma de seus dígitos.
+        Exemplo: 
+        soma_digitos(1234) → 1 + 2 + 3 + 4 = 10.
+    
+*/
+
+#include <stdio.h>
+
+
+int main() {
+
+
+    return 0;
+    
+}

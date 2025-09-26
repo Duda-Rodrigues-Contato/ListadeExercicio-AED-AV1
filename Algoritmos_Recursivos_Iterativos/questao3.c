f11c9d51beff8213171164447a07051582dc4628
Duda Rodrigues <147636488+Duda-Rodrigues-Contato@users.noreply.github.com>
2025-09-26 19:37:25 -0300

    Finalizei questao 3 e inicio da questao 4 (só o escopo).

--- Algoritmos_Recursivos_Iterativos/questao3.c
+++ Algoritmos_Recursivos_Iterativos/questao3.c
@@ -10,9 +10,30 @@
 
 #include <stdio.h>
 
+int soma_digitos(int n) {
+
+    if (n == 0) {
+        return 0;
+    } 
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        return (n % 10) + soma_digitos(n / 10);
+    }
+}
+
 
 int main() {
 
+    int n = 0, somaTotal = 0;
+
+    printf("Digite o bumero: ");
+    scanf("%d", &n);
+
+    somaTotal = soma_digitos(n);
+
+    printf("A soma dos digitos de %d eh: %d\n", n, somaTotal);
 
     return 0;
     
---
+++ Algoritmos_Recursivos_Iterativos/questao4.c
@@ -0,0 +1,19 @@
+/*
+    QUESTÃO 4:
+
+    Implemente uma função recursiva que calcula o produto de dois números
+    inteiros a e b apenas usando somas sucessivas (sem usar *).
+        Exemplo:
+        • produto(5, 3) → 5 + 5 + 5 = 15
+        • produto(7, 2) → 7 + 7 = 14
+    
+*/
+
+#include <stdio.h>
+
+
+int main() {
+
+    return 0;
+    
+}

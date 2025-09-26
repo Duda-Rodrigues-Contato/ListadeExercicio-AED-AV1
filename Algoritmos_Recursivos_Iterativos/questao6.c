2804d940a76205373b9e112fb567f6a5a306474c
Duda Rodrigues <147636488+Duda-Rodrigues-Contato@users.noreply.github.com>
2025-09-26 19:52:31 -0300

    INicio da 6.

---
+++ Algoritmos_Recursivos_Iterativos/questao6.c
@@ -0,0 +1,19 @@
+/*
+    QUESTÃO 6:
+
+    Implemente uma função recursiva que recebe uma string e retorna 1 se for um
+    palíndromo e 0 caso contrário.
+        Exemplo:
+        • palindromo("arara") → 1
+        • palindromo("casa") → 0
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+
+    return 0;
+    
+}

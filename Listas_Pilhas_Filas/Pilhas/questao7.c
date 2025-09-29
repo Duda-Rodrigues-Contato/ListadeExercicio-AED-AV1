f96404b8ac70e4dc113bea7e7813dab38e41e0b1
Duda Rodrigues <147636488+Duda-Rodrigues-Contato@users.noreply.github.com>
2025-09-29 11:17:49 -0300

    Finalizei questão 7 de pilha.

--- Listas_Pilhas_Filas/Pilhas/questao7.c
+++ Listas_Pilhas_Filas/Pilhas/questao7.c
@@ -6,3 +6,75 @@
 */
 
 #include <stdio.h>
+
+
+typedef struct No {
+
+    char dado;
+    struct No* prox;
+
+} No;
+
+
+No* empilhar(No* topo, char valor) { //pilha, palavra[i]
+
+    No* novo = (No*) malloc(sizeof(No));
+
+    novo -> dado = valor;
+    novo -> prox = topo;
+
+    return novo;
+
+}
+
+No* desempilhar(No* topo, char* valor) { // pilha, carac
+
+    if (topo == NULL)
+        return NULL;
+
+    No* temp = topo;
+    *valor = topo -> dado;
+    topo = topo -> prox;
+
+    free(temp);
+    return topo;
+
+}
+
+
+void inverter(char palavra[]) {
+
+    No* pilha = NULL; // Criando a pilha vazia
+
+    for (int i = 0; palavra[i] != '\0'; i++) {
+        pilha = empilhar(pilha, palavra[i]);
+    }
+
+    printf("A palavra invertida eh: \n");
+
+    while (pilha != NULL) {
+
+        char carac;
+
+        pilha = desempilhar(pilha, &carac);
+        printf("%c", carac);
+
+    }
+
+    printf("\n");
+
+}
+
+
+int main() {
+
+    char palavra[50];
+
+    printf("Digite a palavra: ");
+    scanf(" %[^\n]", palavra);
+
+    inverter(palavra);
+
+    return 0;
+
+}

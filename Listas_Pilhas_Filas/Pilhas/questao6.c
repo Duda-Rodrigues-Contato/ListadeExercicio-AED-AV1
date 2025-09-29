b1075c80f9b41adafae88442e9ae65836ac17a70
Duda Rodrigues <147636488+Duda-Rodrigues-Contato@users.noreply.github.com>
2025-09-29 10:30:29 -0300

    Construindo o ambiente de listas (pasta + arquivo questão 1), pilhas (pasta + arquivo questão e, 6 e 7) e filas.

---
+++ Listas_Pilhas_Filas/Pilhas/questao6.c
@@ -0,0 +1,13 @@
+/*
+    QUESTÃO 6 - Verificar se uma Expressão com Parênteses Está Correta:
+
+    Crie uma função que recebe uma string contendo ( e ) e verifica se os
+    parênteses estão balanceados.
+    
+        Exemplo:
+        entrada: "(())()" → saída: Válido
+        entrada: "(()" → saída: Inválido
+
+*/
+
+#include <stdio.h>
---
+++ Listas_Pilhas_Filas/Pilhas/questao7.c
@@ -0,0 +1,8 @@
+/*
+    QUESTÃO 7 - Inverter uma String Usando uma Pilha:
+
+    Implemente um programa que use uma pilha para inverter uma string.
+
+*/
+
+#include <stdio.h>

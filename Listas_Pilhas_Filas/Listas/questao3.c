c40025fb4cab57fcac10496fffd203e45e3a56e0
Duda-Rodrigues-Contato <mdudarodrigues.contato@gmail.com>
2025-10-02 19:18:35 -0300

    Inicio da construção da questão 3 de lista.

---
+++ Listas_Pilhas_Filas/Listas/questao3.c
@@ -0,0 +1,26 @@
+/*
+    QUESTÃO 3 - Inserção Ordenada na Lista Encadeada:
+
+    Altere a implementação da lista encadeada para que os elementos sejam
+    inseridos em ordem crescente automaticamente.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct No {
+
+    int dado;
+    struct No* prox;
+
+} No;
+
+
+int main() {
+
+
+    return 0;
+    
+}

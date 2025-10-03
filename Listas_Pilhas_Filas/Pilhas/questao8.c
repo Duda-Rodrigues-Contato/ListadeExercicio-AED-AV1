66f72cecdd8a45bb95b48084321d0288c19d0297
Duda-Rodrigues-Contato <mdudarodrigues.contato@gmail.com>
2025-10-03 12:43:49 -0300

    Criando a base da questão 8 de pilha.

---
+++ Listas_Pilhas_Filas/Pilhas/questao8.c
@@ -0,0 +1,29 @@
+/*
+    QUESTÃO 8 - Converter um Número Decimal para Binário Usando Pilha:
+
+    Crie um programa que converta um número decimal em binário usando uma
+    pilha.
+    
+    Exemplo:
+        entrada: 10 → saída: 1010
+        entrada: 25 → saída: 11001
+
+*/
+
+#include <stdio.h>
+
+typedef struct No {
+
+    char dado;
+    struct No* prox;
+
+} No;
+
+
+int main() {
+
+
+
+    return 0;
+    
+}
